1f1862bb90d288380db91499ab0dd44a4d7069ce
mariusionut03 <49335973+mariusionut03@users.noreply.github.com>
2021-05-05 12:12:16 +0300

    Update 0.004 (Firebase)
    1. Added Firebase Database.
    2. Added Firebase Hosting (https://lonely-cattos.web.app).

--- public/Scripts/sideMenuScript.js
+++ public/Scripts/sideMenuScript.js
@@ -1,4 +1,23 @@
+var database;
 
+function setup()
+{
+	var firebaseConfig = {
+    	apiKey: "AIzaSyAuUSz4qWUPoiPrABtM7Hg5gR8b5McZfok",
+    	authDomain: "lonely-cattos.firebaseapp.com",
+    	databaseURL: "https://lonely-cattos-default-rtdb.europe-west1.firebasedatabase.app",
+    	projectId: "lonely-cattos",
+    	storageBucket: "lonely-cattos.appspot.com",
+    	messagingSenderId: "34557275051",
+    	appId: "1:34557275051:web:b977924371b71e45fa80ee",
+    	measurementId: "G-MPQQRE7FHR"
+  	};
+  	// Initialize Firebase
+  	firebase.initializeApp(firebaseConfig);
+  	firebase.analytics();
+
+  	database = firebase.database();
+}
 
 function openIndex()
 {
@@ -56,6 +75,14 @@ function minimizeSideNavigation()
 	var sideMenuSmall = document.getElementById("sideMenuSmall");
 	sideMenu.style.width = "0";
 	sideMenuSmall.style.width = "70px"
+	var data = {
+		name: "Mariusel",
+		sidenav: 0
+	}
+
+	console.log(data);
+  	var ref = database.ref('settings');
+  	ref.push(data);
 }
 
 function maximizeSideNavigation()
@@ -64,4 +91,14 @@ function maximizeSideNavigation()
 	var sideMenuSmall = document.getElementById("sideMenuSmall");
 	sideMenu.style.width = "180px";
 	sideMenuSmall.style.width = "0"
+	var data = {
+		name: "Mariusel",
+		sidenav: 1
+	}
+
+	console.log(data);
+  	var ref = database.ref('settings');
+  	ref.push(data);
 }
+
+setup();
