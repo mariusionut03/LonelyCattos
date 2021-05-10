d65335df00c063c4320203a0b80322458847eb94
mariusionut03 <49335973+mariusionut03@users.noreply.github.com>
2021-05-10 20:06:54 +0300

    Update 0.006
    1. Added database.js and organized the scripts.
    2. Added Firebase Authentication. The Sign Up form can now be submitted and an account with email and password will be added to the database.
    3. You can logout now with the button.

---
+++ public/Scripts/database.js
@@ -0,0 +1,34 @@
+var database;
+
+var firebaseConfig = {
+	apiKey: "AIzaSyAuUSz4qWUPoiPrABtM7Hg5gR8b5McZfok",
+	authDomain: "lonely-cattos.firebaseapp.com",
+	databaseURL: "https://lonely-cattos-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "lonely-cattos",
+	storageBucket: "lonely-cattos.appspot.com",
+	messagingSenderId: "34557275051",
+	appId: "1:34557275051:web:b977924371b71e45fa80ee",
+	measurementId: "G-MPQQRE7FHR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// make auth and firestore references
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+// update firestore settings
+db.settings({ timestampsInSnapshots: true});
+
+/*
+Write data in Firebase
+var data = {
+		name: "Mariusel",
+		sidenav: 1
+	}
+
+	console.log(data);
+  	var ref = database.ref('settings');
+  	ref.push(data);
+*/
--- public/Scripts/sideMenuScript.js
+++ public/Scripts/sideMenuScript.js
@@ -1,32 +1,9 @@
-var database;
-
 document.addEventListener('DOMContentLoaded', function() {
 	var modals = document.querySelectorAll('.modal');
 	M.Modal.init(modals);
 	//var items = document.querySelectorAll('.collapsible');
 	//M.Collapsible.init(items);
 });
-
-
-
-function setup()
-{
-	var firebaseConfig = {
-    	apiKey: "AIzaSyAuUSz4qWUPoiPrABtM7Hg5gR8b5McZfok",
-    	authDomain: "lonely-cattos.firebaseapp.com",
-    	databaseURL: "https://lonely-cattos-default-rtdb.europe-west1.firebasedatabase.app",
-    	projectId: "lonely-cattos",
-    	storageBucket: "lonely-cattos.appspot.com",
-    	messagingSenderId: "34557275051",
-    	appId: "1:34557275051:web:b977924371b71e45fa80ee",
-    	measurementId: "G-MPQQRE7FHR"
-  	};
-  	// Initialize Firebase
-  	firebase.initializeApp(firebaseConfig);
-  	firebase.analytics();
-
-  	database = firebase.database();
-}
 
 function openIndex()
 {
@@ -84,14 +61,6 @@ function minimizeSideNavigation()
 	var sideMenuSmall = document.getElementById("sideMenuSmall");
 	sideMenu.style.width = "0";
 	sideMenuSmall.style.width = "70px"
-	var data = {
-		name: "Mariusel",
-		sidenav: 0
-	}
-
-	console.log(data);
-  	var ref = database.ref('settings');
-  	ref.push(data);
 }
 
 function maximizeSideNavigation()
@@ -100,14 +69,4 @@ function maximizeSideNavigation()
 	var sideMenuSmall = document.getElementById("sideMenuSmall");
 	sideMenu.style.width = "180px";
 	sideMenuSmall.style.width = "0"
-	var data = {
-		name: "Mariusel",
-		sidenav: 1
-	}
-
-	console.log(data);
-  	var ref = database.ref('settings');
-  	ref.push(data);
 }
-
-setup();
