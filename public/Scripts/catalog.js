2611574c6b4ffb0a4b705fb00f832127978647d4
mariusionut03 <49335973+mariusionut03@users.noreply.github.com>
2021-05-13 10:27:44 +0300

    Update 0.009
    1. Added a modal for "Create new project" button.
    2. Added 2 fields in Firestore for the collapsible list (githublink and weblink).
    3. The content in the list now shows 2 links (only if they exist).

--- public/Scripts/catalog.js
+++ public/Scripts/catalog.js
@@ -5,10 +5,29 @@ const setupGuides = (data) => {
     let html = '';
     data.forEach(doc => {
         const guide = doc.data();
+        let linksHtml;
+        if(guide.githublink && guide.weblink)
+        {
+            linksHtml = `<a target="_blank" href="${guide.githublink}">GitHub</a> &ensp; <a target="_blank" href="${guide.weblink}">WebLink</a>`;
+        }
+        else if(guide.githublink)
+        {
+            linksHtml = `<a target="_blank" href="${guide.githublink}">GitHub</a>`;
+        }
+        else if(guide.weblink)
+        {
+            linksHtml = `<a target="_blank" href="${guide.weblink}">WebLink</a>`;
+        }
+        else
+        {
+            linksHtml = ``;
+        }
         const li = `
             <li>
-                <div class="collapsible-header lighten-4">${guide.Title}</div>
-                <div class="collapsible-body">${guide.Content}</div>
+                <div class="collapsible-header lighten-4">${guide.title}</div>
+                <div class="collapsible-body">${guide.content}<br>
+                    ${linksHtml}
+                </div>
             </li>
         `;
         html += li;
